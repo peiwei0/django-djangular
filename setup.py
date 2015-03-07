4952c40f68f24ec48be8b2aa4af0ec56a9858173
PEI-WEI <peiwei.wu@gmail.com>
2015-03-07 14:30:47 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='django-djangular',
+    packages=['django-djangular'],  # this must be the same as the name above
+    version='0.1',
+    description='Let django and angular work together',
+    author='Pei-Wei Wu',
+    author_email='peiwei.wu@gmail.com',
+    url='https://github.com/peiwei0/django-djangular',  # use the URL to the github repo
+    download_url='https://github.com/peiwei0/django-djangular/tarball/0.1',
+    keywords=['django', 'angularjs', ],
+    classifiers=[],
+)
